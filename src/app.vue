f86e6fe68478d672885e0a47517768c33ad95cee
Timo Schwarzer <me@timoschwarzer.com>
2020-03-20 23:30:07 +0100

    Initial commit

---
+++ src/app.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title>W27</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    data: () => ({
+      //
+    }),
+  }
+</script>
