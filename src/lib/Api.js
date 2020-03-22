d1034daf39c9b9c4125c7450a3a54b4ad30d108f
Martin Jaskulla <martin.jaskulla@accenture.com>
2020-03-22 13:43:11 +0100

    Fix: Destructer data and remove duplicated method

--- src/lib/Api.js
+++ src/lib/Api.js
@@ -22,7 +22,8 @@ export class Api {
    * }>}
    */
   static async getPlaceById(placeId) {
-    return await axios.get(`/v1/place/${placeId}`)
+    const {data} = await axios.get(`/v1/place/${placeId}`)
+    return data
   }
 
   /**
@@ -43,8 +44,9 @@ export class Api {
    *  createdAt: string
    * }>>}
    */
-  static async getPlacesWithDetails(latitude, longitude) {
-    return await axios.get(`/v1/places/${latitude}/${longitude}`)
+  async getPlacesWithDetails(latitude, longitude) {
+    const {data} = await axios.get(`/v1/places/${latitude}/${longitude}`)
+    return data
   }
 
   /**
@@ -60,7 +62,8 @@ export class Api {
    * }>>}
    */
   static async getPlacesWithBasics(latitude, longitude, radius = 1500) {
-    return await axios.get(`/v1/vicinity/${latitude}/${longitude}/${radius}`)
+    const {data} = await axios.get(`/v1/vicinity/${latitude}/${longitude}/${radius}`)
+    return data
   }
 
   /**
@@ -72,7 +75,8 @@ export class Api {
    * }>>}
    */
   static async getPlaceSuggestions(query) {
-    return await axios.get(`/v1/autocomplete/${query}`)
+    const {data} = await axios.get(`/v1/autocomplete/${query}`)
+    return data
   }
 
   /**
@@ -84,6 +88,22 @@ export class Api {
    * }>>}
    */
   static async getTags(query) {
-    return await axios.get(`/v1/tags/${query}`)
+    const {data} = await axios.get(`/v1/tags/${query}`)
+    return data
+  }
+
+  static async submitTag(comment, label) {
+    const {data} = await axios.put(`/v1/tag`, {
+      label,
+    })
+    return data
+  }
+
+  static async submitComment(placeId, comment, tags = []) {
+    const {data} = await axios.put(`/v1/place/${placeId}/comment`, {
+      content: comment,
+      tags,
+    })
+    return data
   }
 }
